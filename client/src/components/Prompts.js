9a9d17f7e91529c0ba263d47f9468305964cbcdc
Vitaly <molochkovetskiy@gmail.com>
2023-10-02 10:56:19 +0300

    added search for prompts

--- client/src/components/Prompts.js
+++ client/src/components/Prompts.js
@@ -3,12 +3,20 @@ import { useEffect, useState } from 'react';
 
 const Prompts = () => {
     const [prompts, setPrompts] = useState([]);
+    const [search, setSearch] = useState('');
+    const [noResults, setNoResults] = useState(false);
 
     useEffect(() => {
-        allPrompts();
+        fetchPrompts();
     }, []);
 
-    const allPrompts = async () => {
+    useEffect(() => {
+        handleSearch();
+    }, [search]);
+
+    console.log('prompts:', prompts);
+
+    const fetchPrompts = async () => {
         try {
             const response = await fetch('/prompts');
 
@@ -18,25 +26,56 @@ const Prompts = () => {
 
             const data = await response.json();
             setPrompts(data);
+            setNoResults(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleSearch = async () => {
+        try {
+            const response = await fetch(`/prompts/search?description=${search}`);
+            const data = await response.json();
+
+            if (data.msg === 'Prompt not found') {
+                setNoResults(true);
+                setPrompts([]);
+            } else {
+                setPrompts(data);
+                setNoResults(false);
+            }
         } catch (error) {
             console.log(error);
         }
     };
 
     return (
-        <div className="prompts-container">
-            {prompts.map((prompt, index) => (
-                <Link to={`/prompts/${prompt.id}`} key={prompt.id} className="prompt-link">
-                    <div className="prompt-card">
-                        <div className="prompt-content">
-                            <p>{prompt.description}</p>
-                            <p>Your answer: </p>
-                        </div>
-                        <div className="page-number">{index + 1}</div>
-                    </div>
-                </Link>
-            ))}
-        </div>
+        <>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search by description"
+                    onChange={(event) => setSearch(event.target.value)}
+                />
+            </div>
+            <div className="prompts-container">
+                {noResults ? (
+                    <p>No prompts match your search criteria.</p>
+                ) : (
+                    prompts.map((prompt, index) => (
+                        <Link to={`/prompts/${prompt.id}`} key={prompt.id} className="prompt-link">
+                            <div className="prompt-card">
+                                <div className="prompt-content">
+                                    <p>{prompt.description}</p>
+                                    <p>Your answer: </p>
+                                </div>
+                                <div className="page-number">{index + 1}</div>
+                            </div>
+                        </Link>
+                    ))
+                )}
+            </div>
+        </>
     );
 }
 
