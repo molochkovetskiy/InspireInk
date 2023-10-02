4314062d0d435c9072306210eddc012bb6777d61
Vitaly <molochkovetskiy@gmail.com>
2023-10-02 15:54:13 +0300

    created registration and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 
 import Prompts from './components/Prompts';
 import PromptDetails from './components/PromptDetails';
+import LoginRegister from './components/LoginRegister';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path="/prompts" element={<Prompts />} />
-          <Route path="/prompts/:id" element={<PromptDetails />} />
+        <Route path="/prompts" element={<Prompts />} />
+        <Route path="/prompts/:id" element={<PromptDetails />} />
+        <Route path='/login' element={<LoginRegister  title='Login'/>} />
+        <Route path='/register' element={<LoginRegister  title='Register'/>} />
       </Routes>
     </div>
   );
---
+++ client/src/components/LoginRegister.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+function LoginRegister(props) {
+    const [formData, setFormData] = useState({ username: '', password: '' });
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (props.title === 'Register') {
+            try {
+                const response = await fetch('/users/register', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(formData),
+                });
+
+                if (response.status === 200) {
+                    navigate('/login');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            try {
+                const response = await fetch('/users/login', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(formData),
+                });
+
+                if (response.status === 200) {
+                    navigate('/prompts');
+                } else {
+                    // Handle login failure
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                    />
+                </div>
+                <button type="submit">{props.title}</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginRegister;
