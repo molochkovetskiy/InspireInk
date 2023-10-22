1de392b8633204f009dbb2993de2e18d914caec7
Vitaly <molochkovetskiy@gmail.com>
2023-10-22 05:20:22 +0300

    changes for project deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const cookieParser = require('cookie-parser');
@@ -32,4 +33,13 @@ const PORT = process.env.PORT || 5000;
 // Start the Express server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
